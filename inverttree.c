b6e7225c0228b2065de918d8b76fed5ad420a690
thirtiseven lee <ntlihy@gmail.com>
2015-06-13 10:10:40 +0800

    ADD

---
+++ inverttree.c
@@ -0,0 +1,17 @@
+
+
+ struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode* invertTree(struct TreeNode* root) {
+    if(root==NULL)
+        return;
+    struct TreeNode *tmp = root->left;
+    root->left = invertTree(root->right);
+    root->right = invertTree(tmp);
+    return root;
+}
+
